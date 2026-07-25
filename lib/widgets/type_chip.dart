41233afe2b815265b8bb16dff1356be4be1cd41c
apico_nequi <apico@nequi.com>
2026-07-25 18:43:03 -0500

    fix: correct TypeChip colors to use English type names

--- lib/widgets/type_chip.dart
+++ lib/widgets/type_chip.dart
@@ -10,24 +10,50 @@ class TypeChip extends StatelessWidget {
     return Chip(
       label: Text(type),
       backgroundColor: _colorDelTipo(type),
-      // cambiua el radius
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(50),
+        borderRadius: BorderRadius.circular(12),
       ),
     );
   }
 
   static Color _colorDelTipo(String tipo) {
     switch (tipo.toLowerCase()) {
-      case 'fuego':
+      case 'fire':
         return Colors.red.shade300;
-      case 'agua':
+      case 'water':
         return Colors.blue.shade300;
-      case 'planta':
+      case 'grass':
         return Colors.green.shade300;
-      case 'eléctrico':
-      case 'electrico':
+      case 'electric':
         return Colors.yellow.shade600;
+      case 'ice':
+        return Colors.cyan.shade200;
+      case 'fighting':
+        return Colors.orange.shade700;
+      case 'poison':
+        return Colors.purple.shade300;
+      case 'ground':
+        return Colors.brown.shade300;
+      case 'flying':
+        return Colors.indigo.shade100;
+      case 'psychic':
+        return Colors.pink.shade300;
+      case 'bug':
+        return Colors.lightGreen.shade400;
+      case 'rock':
+        return Colors.brown.shade400;
+      case 'ghost':
+        return Colors.deepPurple.shade300;
+      case 'dragon':
+        return Colors.indigo.shade400;
+      case 'dark':
+        return Colors.blueGrey.shade600;
+      case 'steel':
+        return Colors.blueGrey.shade300;
+      case 'fairy':
+        return Colors.pinkAccent.shade100;
+      case 'normal':
+        return Colors.grey.shade400;
       default:
         return Colors.grey.shade300;
     }
