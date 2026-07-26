1039d984c18076fe9bca3b8f4a2cee6f86a3ccc2
apico_nequi <apico@nequi.com>
2026-07-25 19:13:55 -0500

    style: customize app theme (seed color, rounded cards, centered title)

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static const seed = Color(0xFFE3350D); // rojo Pokédex
+  static const seed = Color(0xFF00E676);
 
   static final ThemeData light = ThemeData(
     colorScheme: ColorScheme.fromSeed(
@@ -10,6 +10,17 @@ class AppTheme {
     ),
     textTheme: _textTheme,
     useMaterial3: true,
+    cardTheme: const CardThemeData(
+      elevation: 2,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(40)),
+      ),
+    ),
+    appBarTheme: const AppBarTheme(
+      centerTitle: true,
+      elevation: 0,
+    ),
+
   );
 
   static final ThemeData dark = ThemeData(
@@ -19,10 +30,21 @@ class AppTheme {
     ),
     textTheme: _textTheme,
     useMaterial3: true,
+      cardTheme: const CardThemeData(
+        elevation: 2,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(40)),
+        ),
+      ),
+      appBarTheme: const AppBarTheme(
+        centerTitle: true,
+        elevation: 0,
+      ),
+
   );
 
   static const TextTheme _textTheme = TextTheme(
-    titleLarge: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+    titleLarge: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
     bodyMedium: TextStyle(fontSize: 15),
   );
 }
